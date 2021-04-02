0717bcb8d6d2aba7d74a5521a7b388601ec79062
Dorivaldo Valentim <dorivaladovalentim@gmail.com>
2021-04-02 11:29:05 +0100

    Mudando de jQuery 3.3.1 para jQuery 3.2.1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -102,7 +102,7 @@
         </div>
         
         <!-- Essential javascripts for application to work -->
-        <script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
+        <script src="{{ asset('js/jquery-3.2.1.min.js') }}"></script>
         <script src="{{ asset('js/popper.min.js') }}"></script>
         <script src="{{ asset('js/bootstrap.min.js') }}"></script>
         <script src="{{ asset('js/main.js') }}"></script>

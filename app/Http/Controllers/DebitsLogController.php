7e791a47970cbd9f9d1a262cb85937da2ba267a6
Dorivaldo Valentim <dorivaladovalentim@gmail.com>
2021-12-04 01:05:56 +0100

    Trabalhando a lógica de before_transation & after_transation em debits_logs

--- app/Http/Controllers/DebitsLogController.php
+++ app/Http/Controllers/DebitsLogController.php
@@ -46,6 +46,8 @@ class DebitsLogController extends Controller
         $debits_log->amount = $request->amount;
         $debits_log->type = $request->type;
         $debits_log->description = $request->description;
+        $debits_log->before_transation = $debit->amount;
+        $debits_log->after_transation = $request->type == "I" ? $debit->amount - $request->amount : $debit->amount + $request->amount;
 
         if ($debits_log->save()) {
             if ($request->type == 'I') {
